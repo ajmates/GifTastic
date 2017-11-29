e3541b05dc3ba184fc62863333adcb9a6020ce14
Amanda Mates <mates.j.amanda@gmail.com>
2017-11-29 18:12:40 -0500

    push

---
+++ assets/javascript/gif.js
@@ -0,0 +1,8 @@
+// jquery tag
+src="http://code.jquery.com/jquery-2.1.3.min.js" 
+// begin with an array of animals ; create div for them to be stored
+var animals = ["Dog", "Cat", "Rabbit", "Skunk"]; 
+// for loop to go through the length of the array i = 0; >>create buttons 
+// when a new animal is put into the search box, add to the array >> dynamicaly create button 
+// ajax call 
+// when button is clicked >> populate page with gifs 
